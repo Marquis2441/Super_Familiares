f630f20e9212bb5aea022494c86af1fcf91e5a12
Marcos <marcosabelrossi@gmail.com>
2022-09-22 19:20:37 -0300

    Se agregan app familiar. Primera migracion de familiares

--- Family/models.py
+++ Family/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class SuperFamiliares(models.Model):
+    nombre = models.CharField(max_length=128)
+    fecha_nacimiento = models.DateField()
+    peso_kg = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.nombre}'
+
+    pass
